102419200ab98596488a25e28d8bd94c45c87b4c
heytitle <pat.chormai@gmail.com>
2019-01-22 14:24:22 +0100

    fix gpu issue

--- src/models.py
+++ src/models.py
@@ -78,6 +78,7 @@ class Squeezenet(Model):
         self.emb_size = emb_size
         self.model = tvm.squeezenet1_1(pretrained=True)
 
+        self.features = self.model.features.to(utils.torch_device())
         self.classifier = nn.Sequential(
             *list(self.model.classifier.children())[:4]).to(utils.torch_device()
         )
@@ -85,7 +86,7 @@ class Squeezenet(Model):
         # self.features = nn.Sequential(*list(self.model.children())[:-3]).to(utils.torch_device())
     def forward_pass(self, batch):
 
-        x = self.model.features(batch)
+        x = self.features(batch)
         x = self.classifier(x)
 
         return x.view(batch.size(0), -1)
